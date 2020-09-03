89ead506313bbf517def95567fbcf0e36bb8c6af
Bodei Brouwer <bodeibrouwer@gmail.com>
2020-09-03 16:21:03 +0200

    my page first attempt

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -25,31 +25,28 @@ export default class UserDetails extends React.Component {
       return <p>Loading .. </p>
     }
 
-    const {name, category, description, _id, creator} = this.state.games
-
     return (
       <div className='form-page' >
       {
         !this.props.loggedInUser ? <p>Sign in <Link to="/login">here</Link></p> : 
         <div className='form-field'>
-        <h1>{name}</h1>
+        <h1>{this.state.games[0].creator.username}'s games</h1>
+        {/* <h1>{this.state.games.creator._id}</h1> */}
         {
           !this.state.games ? (<p>Loading ... </p>) : 
           this.state.games.length === 0 ? null :
           <>
-            <hr/>
-            <h4>{creator._id}</h4>
           {
             this.state.games.map((game, i) => {
             return (
             <>
-            <Card key={"mygame"+i}>
-              <Card.Header className='card-header'><span className='card-title'><h5>{name}</h5> <h5>{category.charAt(0).toUpperCase() + category.slice(1)}</h5></span></Card.Header>
+            <Card key={"usergame"+game._id}>
+              <Card.Header className='card-header'><span className='card-title'><h5>{game.name} |</h5> <h5>{game.category.charAt(0).toUpperCase() + game.category.slice(1)}</h5></span></Card.Header>
               <Card.Body>
                 <Card.Text className='card-game-descr overflow'>
-                  {description}
+                  {game.description}
                 </Card.Text>
-                <Link to={`/games/${_id}`}>
+                <Link to={`/games/${game._id}`}>
                   <Button variant="btn btn-success">More</Button>
                 </Link>
             </Card.Body>
